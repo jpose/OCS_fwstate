5a09215597efc39421292ceb76caffc5205df659
jpose <pose.jeremy@laposte.net>
2016-08-12 09:13:43 +0200

    Create fwstate.php

---
+++ sql/fwstate.php
@@ -0,0 +1,13 @@
+<?php
+
+// Create table
+$object -> sql_query("CREATE TABLE IF NOT EXISTS `fwstate` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `HARDWARE_ID` int(11) NOT NULL,
+  `DOMAINSTATE` varchar(255) DEFAULT NULL,
+  `PRIVATESTATE` varchar(255) DEFAULT NULL,
+  `PUBLICSTATE` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ID`,`HARDWARE_ID`)
+)  ENGINE=INNODB ;");
+
+?>
